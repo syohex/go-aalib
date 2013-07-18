e9a4fc53237c712800c372722c012207d5c1009b
Syohei YOSHIDA <syohex@gmail.com>
2013-07-18 21:18:21 +0900

    Add tests

--- aalib_test.go
+++ aalib_test.go
@@ -19,12 +19,56 @@ func TestClose(t *testing.T) {
 	handle.Close()
 }
 
+func TestFlush(t *testing.T) {
+	handle, _ := Init(100, 100, AA_NORMAL)
+	if handle == nil {
+		t.Errorf("Return nil from constructor")
+	}
+	handle.Flush()
+}
+
+func TestResize(t *testing.T) {
+	handle, _ := Init(100, 100, AA_NORMAL)
+	if handle == nil {
+		t.Errorf("Return nil from constructor")
+	}
+	ret, _ := handle.Resize()
+	if ret == -1 {
+		t.Errorf("Failed: Resize()")
+	}
+}
+
+// Difficult tests for Text(), Attrs(), Image()
+
+func TestScrHeight(t *testing.T) {
+	handle, _ := Init(100, 100, AA_NORMAL)
+	if handle == nil {
+		t.Errorf("Return nil from constructor")
+	}
+	scrwidth := handle.ScrHeight()
+	if scrwidth == 0 {
+		t.Errorf("Error")
+	}
+}
+
+
 func TestImgWidth(t *testing.T) {
 	handle, _ := Init(100, 100, AA_NORMAL)
 	if handle == nil {
 		t.Errorf("Return nil from constructor")
 	}
-	scrwidth := handle.ImgWidth()
+	scrwidth := handle.ScrHeight()
+	if scrwidth == 0 {
+		t.Errorf("Error")
+	}
+}
+
+func TestImgHeight(t *testing.T) {
+	handle, _ := Init(100, 100, AA_NORMAL)
+	if handle == nil {
+		t.Errorf("Return nil from constructor")
+	}
+	scrwidth := handle.ImgHeight()
 	if scrwidth == 0 {
 		t.Errorf("Error")
 	}
